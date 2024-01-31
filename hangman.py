872b242e01c67d6f2827f57854ecbe42a39e67b8
Thiago Marvin <thiagomarvin.soares@gmail.com>
2024-01-31 15:13:44 -0300

    Sending the letter review

--- hangman.py
+++ hangman.py
@@ -1,1 +1,19 @@
 word_list = ["ardvack","baboon","camel"]
+import random
+
+chosen_word = random.choice(word_list)
+print(f'Pssst, the solution is {chosen_word}.')
+display = []
+for _ in chosen_word:
+    display += '_'
+
+print(display)
+
+guess = input('Guess a letter: ').lower()
+
+for position in range(len(chosen_word)):
+    letter = chosen_word[position]
+    if letter in guess:
+        display[position] = letter
+
+print(display)
