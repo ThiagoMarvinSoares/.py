a3e02e85907cdf933b2a564989191c47e3bf41f8
Thiago Marvin <thiagomarvin.soares@gmail.com>
2024-02-09 14:23:24 -0300

    sending training with lists and dictionarys

--- main.py
+++ main.py
@@ -79,4 +79,64 @@ import random
 #    print(f'World, {a} from {b}')
 #    print(f'Helo, {a}  from {b}')
 
-#greet_with(a='Thiago Marvin', b='Miramar')    
+#greet_with(a='Thiago Marvin', b='Miramar') 
+
+#----------Py Dictionary----------
+
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+}   
+
+#print(programming_dictionary["Bug"])
+
+
+
+
+#----------Adding new items to dictionary----------
+programming_dictionary["Loop"] = "The action of doing something over and over again"
+
+#----------Create an empty dictionary----------
+empty_dictionary = {}
+
+#----------Wipe an existing dictionary----------
+#programming_dictionary = {}
+
+#----------Edit an item in a existing dictionary----------
+programming_dictionary["Bug"] = 'A moth in your computer'
+
+#---------Loop through a dictionary---------
+# for key in programming_dictionary:
+#    print(key)
+#    print(programming_dictionary[key])
+
+#----------Nested Lists and dictionary----------
+ #----------Nested dictionary----------
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+ #----------Nested Lists----------
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lillie", "Dijon"], "total_visits": 2},
+}
+#----------Nested Lists in a and dictionary----------
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lillie", "Dijon"], "total_visits": 2},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "times_visited": 5}
+}
+#print(travel_log)
+
+#Nesting dictionary in  a list
+travel_log = [
+    {
+      "country": "France",
+      "cities_visited": ["Paris", "Lillie", "Dijon"],
+      "total_visits": 2
+    },
+    {
+      "country":"Germany",
+      "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+      "times_visited": 5
+    },
+]
